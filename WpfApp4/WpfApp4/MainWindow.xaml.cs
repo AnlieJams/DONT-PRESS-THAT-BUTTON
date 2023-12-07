17bb033442022e3d152fdfecdf3e0cc9f8b3bffe
AnlieJams <98762736+AnlieJams@users.noreply.github.com>
2023-12-07 04:55:54 +0200

    Add files via upload

---
+++ WpfApp4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int value = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            value++;
+            if (value == 1)
+            {
+                MessageBox.Show($"Опа, вы издали звук ЫЫЫЫЫЭЭЭЭЭЭЭА: {value} раз");
+            }
+            else MessageBox.Show($"Опа, вы издали звук ЫЫЫЫЫЭЭЭЭЭЭЭА: {value} раза");
+            if (value == 4)
+            {
+                MessageBox.Show("так, всё харэ");
+            }
+        }
+    }
+}
